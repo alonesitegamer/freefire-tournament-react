a126481031d9b797beefd6fb1e4c6bbbe5522a3e
alonesitegamer <112342699+alonesitegamer@users.noreply.github.com>
2025-11-02 10:56:04 +0530

    Update App.jsx

--- project-root/src/App.jsx
+++ project-root/src/App.jsx
@@ -1,4 +1,4 @@
-Splash React from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
