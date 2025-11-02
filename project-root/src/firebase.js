846e82f17943a24e8c13ee5542c32b172d2104f3
alonesitegamer <112342699+alonesitegamer@users.noreply.github.com>
2025-11-02 10:44:00 +0530

    Update firebase.js

--- project-root/src/firebase.js
+++ project-root/src/firebase.js
@@ -7,15 +7,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyAIckZEyuVFr7aewkCNzIEmxB1uUjGJgEU",
   authDomain: "imperial-esports-da816.firebaseapp.com",
   projectId: "imperial-esports-da816",
-  storageBucket: "imperial-esports-da816.firebasestorage.app",
+  storageBucket: "imperial-esports-da816.appspot.com", // ✅ fixed here
   messagingSenderId: "599688885836",
   appId: "1:599688885836:web:bbd08bb6b1984b45eea6e9",
   measurementId: "G-9D09P3EWRG"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
-
-export { auth, provider, db };
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
