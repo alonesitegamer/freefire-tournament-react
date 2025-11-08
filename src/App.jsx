518b7daae45ddd127937718b53db8726596ffb06
alonesitegamer <priyankabairagi036@gmail.com>
2025-11-08 18:55:34 +0530

    First commit: Initial React app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Imperial eSports</h1>
+        <p>Tournaments start soon!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
