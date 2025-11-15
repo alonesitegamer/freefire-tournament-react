dcc7fc502e2826fc1061a008a21ca2c81fb52914
alonesitegamer <112342699+alonesitegamer@users.noreply.github.com>
2025-11-15 19:48:49 +0530

    Update AdminPanel.jsx

--- project-root/src/components/AdminPanel.jsx
+++ project-root/src/components/AdminPanel.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function AdminPanel({
   requests = { topup: [], withdraw: [] },
-  approveRequest,
-  rejectRequest,
+  approveRequest = () => {},
+  rejectRequest = () => {},
   matches = []
 }) {
   return (
